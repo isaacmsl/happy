9fe0b1ec9a42b2905f50c944cfe99567989dc425
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-10-13 00:00:42 -0300

    feat(front): ++head map/fix text

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from '../../styles/Map.module.css'
 import Link from 'next/link'
 import { FiPlus } from 'react-icons/fi'
@@ -7,13 +8,24 @@ import LeafletMapDynamic from '../components/LeafletMapDynamic'
 export default function Map() {
     return (
         <div className={styles.mapPage}>
+            <Head>
+                <link rel="icon" href="favicon.ico" />
+                <title>Happy</title>
+                <meta charSet="UTF-8 " />
+                <meta name="author" content="Isaac Lourenço" />
+                <meta name="description" content="Incentivando visitas a orfanatos de Santa Cruz, Rio Grande do Norte" />
+                <meta name="keywords" content="orfanato, santa cruz rn" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="content-language" content="pt-br" />
+            </Head>
+
             <aside>
                 <header>
                     <img src="happy-marker.svg" alt="Happy" />
 
                     <div className={styles.headerText}>
                         <h2>Escolha um orfanato no mapa</h2>
-                        <p>Muitas crinças estão esperando sua visita {':)'}</p>
+                        <p>Muitas crianças estão esperando sua visita {':)'}</p>
                     </div>
                 </header>
 
