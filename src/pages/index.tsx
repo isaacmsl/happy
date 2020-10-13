cb0a6b0280592bfff2bfb750a92c24526e02a157
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-10-13 00:00:19 -0300

    feat(front): ++head index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from '../../styles/Landing.module.css'
 import Link from 'next/link'
 import { FiArrowRight } from 'react-icons/fi'
@@ -5,6 +6,17 @@ import { FiArrowRight } from 'react-icons/fi'
 export default function Landing() {
   return (
     <div className={styles.landingPage}>
+      <Head>
+        <link rel="icon" href="favicon.ico" />
+        <title>Happy</title>
+        <meta charSet="UTF-8 " />
+        <meta name="author" content="Isaac Lourenço" />
+        <meta name="description" content="Incentivando visitas a orfanatos de Santa Cruz, Rio Grande do Norte" />
+        <meta name="keywords" content="orfanato, santa cruz rn" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="content-language" content="pt-br" />
+      </Head>
+
       <div className={styles.contentWrapper}>
         <header>
           <img src="happy-marker.svg" alt="Happy" />
